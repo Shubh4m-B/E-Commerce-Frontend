e4deac7a47a833db0219136c5d8d403834cacb9a
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-10-04 20:06:37 +0530

    Set up env variables

--- src/User/SignUp.jsx
+++ src/User/SignUp.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../Core/Layout'
+import { API } from '../config'
 
 export default function SignUp() {
     return (
         <Layout title="Sign Up Page" description="Node React E-Commerce App">
-            This is sign up page
+            Sign up page
+            {API}
+            {console.log("API:", API)}
         </Layout>
     )
 }
