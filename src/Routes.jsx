4b72a1ca8a86bb52f9f05f3993815e325ba2ec4c
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-10-04 16:31:43 +0530

    Set up menu component containing a basic navbar

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './Core/Home'
 import SignIn from './User/SignIn'
 import SignUp from './User/SignUp'
+import Menu from './Core/Menu'
 
 export default function Routes() {
     return (
         <BrowserRouter>
+            <Menu />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={SignIn} />
