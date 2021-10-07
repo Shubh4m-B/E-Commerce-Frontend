fd6615349f713403ee0727e6aa480d1250840d6a
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-10-08 00:17:29 +0530

    Add signup form

--- src/User/SignUp.jsx
+++ src/User/SignUp.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../Core/Layout'
-import { API } from '../config'
+// import { API } from '../config'
 
 export default function SignUp() {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    })
+
+    const handleChange = (name) => (event) => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    }
+
     return (
-        <Layout title="Sign Up Page" description="Node React E-Commerce App">
-            Sign up page
-            {API}
-            {console.log("API:", API)}
+        <Layout title="Sign Up" description="Sign Up to the React E-Commerce App" className="container col-md-4 offset-md-4">
+            <form>
+                <div className="form-group">
+                    <label className="text-muted">Name</label>
+                    <input type="text" className="form-control" onChange={handleChange('name')} />
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Email</label>
+                    <input type="text" className="form-control" onChange={handleChange('email')} />
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Password</label>
+                    <input type="password" className="form-control" onChange={handleChange('password')} />
+                </div>
+                <button className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
         </Layout>
     )
 }
