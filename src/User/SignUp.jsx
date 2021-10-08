0355824a13522b16ed8fba264b307cd5eea02676
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-10-08 18:13:43 +0530

    Add success and error messages in the signup form

--- src/User/SignUp.jsx
+++ src/User/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Layout from '../Core/Layout'
 import { API } from '../config'
 
@@ -11,14 +12,14 @@ export default function SignUp() {
         success: false
     })
 
-    const { name, email, password } = values
+    const { name, email, password, error, success } = values
 
     const handleChange = (name) => (event) => {
         setValues({ ...values, error: false, [name]: event.target.value });
     }
 
     const signup = (user) => {
-        fetch(`${API}/signup`, {
+        return fetch(`${API}/signup`, {
             method: "POST",
             headers: {
                 Accept: "application/json",
@@ -37,6 +38,21 @@ export default function SignUp() {
     const clickSubmit = (event) => {
         event.preventDefault()
         signup({ name, email, password })
+            .then(data => {
+                if (data.error) {
+                    setValues({ ...values, error: data.error, success: false })
+                }
+                else {
+                    setValues({
+                        ...values,
+                        name: "",
+                        email: "",
+                        password: "",
+                        error: "",
+                        success: true
+                    })
+                }
+            })
     }
 
     return (
@@ -44,19 +60,25 @@ export default function SignUp() {
             <form>
                 <div className="form-group">
                     <label className="text-muted">Name</label>
-                    <input type="text" className="form-control" onChange={handleChange('name')} />
+                    <input type="text" className="form-control" onChange={handleChange('name')} value={name} />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">Email</label>
-                    <input type="text" className="form-control" onChange={handleChange('email')} />
+                    <input type="text" className="form-control" onChange={handleChange('email')} value={email} />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">Password</label>
-                    <input type="password" className="form-control" onChange={handleChange('password')} />
+                    <input type="password" className="form-control" onChange={handleChange('password')} value={password} />
                 </div>
-                <button className="btn btn-primary" onClick={clickSubmit}>
+                <button className="btn btn-primary" onClick={clickSubmit} style={{ width: "100%" }}>
                     Submit
                 </button>
+                <div className="alert alert-danger" style={{ display: error ? "" : "none", marginTop: "30px" }}>
+                    {error}
+                </div>
+                <div className="alert alert-info" style={{ display: success ? "" : "none", marginTop: "30px" }}>
+                    New account created successfully. Please <Link to="/signin">Sign in</Link> now
+                </div>
             </form>
         </Layout>
     )
